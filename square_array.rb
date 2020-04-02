66b29572d7c2dc7e6e8fabec89cbfce899edd4e5
Bradley Rush <bradleyj.rush@gmail.com>
2020-04-02 16:45:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,4 @@
 def square_array(array)
-  new_array = array.each {|number| (number * number)}
-  
-    new_array << (number * number)
+  new_array = array.each {|number| (number ** number)}
   end
 end
